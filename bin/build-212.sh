dd18a07a0f8119a425b0a6de0819dd7856d5baec
quapka <quapka@gmail.com>
2019-11-21 08:53:54 +0100

    Add script for partial build of JCE version 212

---
+++ bin/build-212.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# build for 212
+javacard_framework="$HOME/projects/fi/thesis/ext/jc212_kit/lib/api21.jar:"
+src="$1"
+echo "Compiling into *.class files.."
+# source https://stackoverflow.com/a/35470243/2377489
+javac\
+    -g\
+    -source 1.1\
+    -target 1.1\
+    -cp "$javacard_framework"\
+    "$src"
+
+converter="$HOME/projects/fi/thesis/ext/jc212_kit/lib/converter.jar"
+export_path="$HOME/projects/fi/thesis/ext/jc212_kit/api_export_files"
+AID="0xA0:0x00:0x00:0x00:0xAA:0x00:0x01"
+version="1.0"
+echo "Converting into *.cap files.."
+java -jar "$converter" -classdir src -out CAP -exportpath "$export_path" com.JCVersion "$AID" "$version"
---
+++ cards/README.md
@@ -0,0 +1,4 @@
+Ideas
+-----
+Create an applet with a name and a identification (assigning single consequtive letter from the alphabet is enough).
+This way we can programmatically keep track of each card, that is tested.
