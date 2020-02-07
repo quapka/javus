cc5553a7669a175ccf3613921e0184da3ee6f794
quapka <quapka@gmail.com>
2020-02-07 15:31:58 +0100

    Add Python package jcvmutils

---
+++ pkgs/JCVMUtils/jcvmutils/utils.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import os
+import re
+import subprocess as sp
+import sys
+
+log = logging.getLogger(__file__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(name)s: %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+LOG_LEVELS = [
+    logging.DEBUG, logging.INFO, logging.WARNING,
+    logging.ERROR, logging.CRITICAL
+]
+
+class CommandLineApp():
+    '''
+    Template for Python command line applications.
+    '''
+    APP_DESCRIPTION = "Run a command and record it's output in a MongDB"
+
+    def __init__(self):
+        self.verbosity = logging.ERROR
+
+        self.parser = argparse.ArgumentParser(
+                description="Run a command and record it's output in a MongDB"
+        )
+        self.add_options()
+        self.parse_options()
+
+        log.setLevel(self.verbosity)
+        log.debug('Logging level changed')
+
+    def add_options(self):
+        levels = ', '.join([str(lvl) for lvl in LOG_LEVELS])
+        self.parser.add_argument(
+            '-v', '--verbose',
+            help='Set the verbosity {' + levels + '}',
+            type=self.validate_verbosity,
+        )
+
+    def validate_verbosity(self, value):
+        try:
+            value = int(value)
+        except ValueError:
+            raise argparse.ArgumentTypeError('verbosity is not and integer')
+        if value not in LOG_LEVELS:
+            raise argparse.ArgumentTypeError('verbosity level not from expected range')
+        return value
+
+    def parse_options(self):
+        args = self.parser.parse_args()
+        if args.verbose is not None:
+            self.verbosity = args.verbose
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    app = CommandLineApp()
+    app.run()
---
+++ pkgs/JCVMUtils/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jcvmutils',
+version='0.1',
+description='Package containing utilities for the analysis of JavaCard Virtual Machine',
+url='#',
+author='quapka',
+author_email='quapka@gmail.com',
+license='MIT',
+packages=['jcvmutils'],
+zip_safe=False)
