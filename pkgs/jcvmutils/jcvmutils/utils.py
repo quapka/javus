b7b57ffeece89f623edee9f492a150c8db194a09
quapka <quapka@gmail.com>
2020-02-20 11:33:38 +0100

    Simplify code by using packages

--- pkgs/jcvmutils/jcvmutils/utils.py
+++ pkgs/jcvmutils/jcvmutils/utils.py
@@ -5,6 +5,9 @@ import os
 import re
 import subprocess as sp
 import sys
+import time
+
+import pymongo
 
 log = logging.getLogger(__file__)
 handler = logging.StreamHandler()
@@ -16,6 +19,45 @@ LOG_LEVELS = [
     logging.DEBUG, logging.INFO, logging.WARNING,
     logging.ERROR, logging.CRITICAL
 ]
+
+# kudos to: https://medium.com/@ramojol/python-context-managers-and-the-with-statement-8f53d4d9f87
+class MongoConnection(object):
+    def __init__(self, host='localhost', port='27017',
+            database='card-analysis', collation='commands'):
+        self.host = host
+        self.port = port
+        self.connection = None
+        self.db_name = database
+        self.collation_name = collation
+
+    def __enter__(self, *args, **kwargs):
+        conn_str = f'mongodb://{self.host}:{self.port}'
+        log.debug('Starting the connection with %s', conn_str)
+
+        self.connection = pymongo.MongoClient(conn_str)
+        self.db = self.connection[self.db_name]
+        self.col = self.db[self.collation_name]
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        log.debug('Closing the connection to the database')
+        self.connection.close()
+
+class Timer(object):
+    # naive timer, but to get at least an idea
+    def __init__(self):
+        self.start = None
+        self.end = None
+        self.duration = None
+
+    def __enter__(self, *args, **kwargs):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        self.duration = self.end - self.start
+
 
 class CommandLineApp():
     '''
