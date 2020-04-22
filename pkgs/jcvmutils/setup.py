a1482f6eb97b9cad0ecaaf361cf6e62f3fc7618b
quapka <quapka@gmail.com>
2020-04-22 11:50:21 +0200

    Format source code mechanically

--- pkgs/jcvmutils/setup.py
+++ pkgs/jcvmutils/setup.py
@@ -4,15 +4,17 @@ from os import path
 from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='jcvmutils',
-version='0.1',
-description='Package containing utilities for the analysis of JavaCard Virtual Machine',
-long_description=long_description,
-url='#',
-author='quapka',
-author_email='quapka@gmail.com',
-packages=['jcvmutils'],
-zip_safe=False)
+setup(
+    name="jcvmutils",
+    version="0.1",
+    description="Package containing utilities for the analysis of JavaCard Virtual Machine",
+    long_description=long_description,
+    url="#",
+    author="quapka",
+    author_email="quapka@gmail.com",
+    packages=["jcvmutils"],
+    zip_safe=False,
+)
