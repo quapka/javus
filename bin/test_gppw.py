12fabfc85816af33902fc444e2d065dde20ca2d6
quapka <quapka@gmail.com>
2020-04-23 12:51:54 +0200

    Split tests into live and regular

--- bin/test_gppw.py
+++ bin/test_gppw.py
@@ -51,32 +51,29 @@ def test_gp_prefix_with_diversifier(div, str_div):
 # def test_detecting_diversifier_from_config():
 #     raise ValueError
 
-# TODO live tests
-
-
-# FIXME add pytest.ini with custom markers
-# FIXME needs live config
 # FIXME should be reimplemented as pytest check rather
-# @pytest.mark.live
-def test_for_specific_gp_version():
-    config = configparser.ConfigParser()
-    config["PATHS"] = {
-        "gp.jar": "/home/qup/projects/fi/crocs/GlobalPlatformPro/gp.jar",
-    }
-    gp = GlobalPlatformProWrapper(config=config, dry_run=False)
-    gp.process_config()
-
-    gp.read_gp_version()
-    assert gp.version == "GlobalPlatformPro v20.01.23-0-g5ad373b"
 
 
-def test_that_gp_is_working():
-    config = configparser.ConfigParser()
-    config["PATHS"] = {
-        "gp.jar": "/home/qup/projects/fi/crocs/GlobalPlatformPro/gp.jar",
-    }
-    gp = GlobalPlatformProWrapper(config=config, dry_run=False)
-    gp.process_config()
+@pytest.mark.live
+class TestLive:
+    def setup_method(self, method):
+        self.config = configparser.ConfigParser()
+        # FIXME the path needs to be loaded dynamically
+        self.config["PATHS"] = {
+            "gp.jar": "/home/qup/projects/fi/crocs/GlobalPlatformPro/gp.jar",
+        }
 
-    gp.verify_gp()
-    assert gp.works == True
+    @pytest.mark.skip("Takes too long")
+    def test_for_specific_gp_version(self):
+        gp = GlobalPlatformProWrapper(config=self.config, dry_run=False)
+        gp.process_config()
+
+        gp.read_gp_version()
+        assert gp.version == "GlobalPlatformPro v20.01.23-0-g5ad373b"
+
+    def test_that_gp_is_working(self):
+        gp = GlobalPlatformProWrapper(config=self.config, dry_run=False)
+        gp.process_config()
+
+        gp.verify_gp()
+        assert gp.works == True
