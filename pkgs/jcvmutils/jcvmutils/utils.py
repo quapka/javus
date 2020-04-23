fe5f5e8e395f0a7b1c9885c2e046fbbf79644a78
quapka <quapka@gmail.com>
2020-04-23 11:10:10 +0200

    Some auto-formatting of code

--- pkgs/jcvmutils/jcvmutils/utils.py
+++ pkgs/jcvmutils/jcvmutils/utils.py
@@ -11,19 +11,27 @@ import pymongo
 
 log = logging.getLogger(__file__)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(name)s: %(message)s')
+formatter = logging.Formatter("%(levelname)s:%(asctime)s:%(name)s: %(message)s")
 handler.setFormatter(formatter)
 log.addHandler(handler)
 
 LOG_LEVELS = [
-    logging.DEBUG, logging.INFO, logging.WARNING,
-    logging.ERROR, logging.CRITICAL
+    logging.DEBUG,
+    logging.INFO,
+    logging.WARNING,
+    logging.ERROR,
+    logging.CRITICAL,
 ]
 
 # kudos to: https://medium.com/@ramojol/python-context-managers-and-the-with-statement-8f53d4d9f87
 class MongoConnection(object):
-    def __init__(self, host='localhost', port='27017',
-            database='card-analysis', collation='commands'):
+    def __init__(
+        self,
+        host="localhost",
+        port="27017",
+        database="card-analysis",
+        collation="commands",
+    ):
         self.host = host
         self.port = port
         self.connection = None
@@ -31,8 +39,8 @@ class MongoConnection(object):
         self.collation_name = collation
 
     def __enter__(self, *args, **kwargs):
-        conn_str = f'mongodb://{self.host}:{self.port}'
-        log.debug('Starting the connection with %s', conn_str)
+        conn_str = f"mongodb://{self.host}:{self.port}"
+        log.debug("Starting the connection with %s", conn_str)
 
         self.connection = pymongo.MongoClient(conn_str)
         self.db = self.connection[self.db_name]
@@ -40,8 +48,9 @@ class MongoConnection(object):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        log.debug('Closing the connection to the database')
+        log.debug("Closing the connection to the database")
         self.connection.close()
+
 
 class Timer(object):
     # naive timer, but to get at least an idea
@@ -59,30 +68,30 @@ class Timer(object):
         self.duration = self.end - self.start
 
 
-class CommandLineApp():
-    '''
+class CommandLineApp(object):
+    """
     Template for Python command line applications.
-    '''
+    """
+
     APP_DESCRIPTION = None
 
     def __init__(self):
         self.verbosity = logging.ERROR
         self.args = None
 
-        self.parser = argparse.ArgumentParser(
-                description=self.APP_DESCRIPTION,
-        )
+        self.parser = argparse.ArgumentParser(description=self.APP_DESCRIPTION,)
         self.add_options()
         self.parse_options()
 
         log.setLevel(self.verbosity)
-        log.debug('Logging level changed')
+        log.debug("Logging level changed")
 
     def add_options(self):
-        levels = ', '.join([str(lvl) for lvl in LOG_LEVELS])
+        levels = ", ".join([str(lvl) for lvl in LOG_LEVELS])
         self.parser.add_argument(
-            '-v', '--verbose',
-            help='Set the verbosity {' + levels + '}',
+            "-v",
+            "--verbose",
+            help="Set the verbosity {" + levels + "}",
             type=self.validate_verbosity,
         )
 
@@ -90,9 +99,9 @@ class CommandLineApp():
         try:
             value = int(value)
         except ValueError:
-            raise argparse.ArgumentTypeError('verbosity is not and integer')
+            raise argparse.ArgumentTypeError("verbosity is not and integer")
         if value not in LOG_LEVELS:
-            raise argparse.ArgumentTypeError('verbosity level not from expected range')
+            raise argparse.ArgumentTypeError("verbosity level not from expected range")
         return value
 
     def parse_options(self):
@@ -101,9 +110,9 @@ class CommandLineApp():
             self.verbosity = self.args.verbose
 
     def run(self):
-        raise RuntimeError('Not implemented yet!')
-        pass
+        raise NotImplementedError("The method 'run' has not bee implemented!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = CommandLineApp()
     app.run()
