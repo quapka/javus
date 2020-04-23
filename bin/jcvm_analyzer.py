97e165b18acc278181d8e9b24b0510e8a376bb9d
quapka <quapka@gmail.com>
2020-04-24 00:02:53 +0200

    Add CardState

--- bin/jcvm_analyzer.py
+++ bin/jcvm_analyzer.py
@@ -13,6 +13,7 @@ import platform
 import sys
 import enum
 import logging
+import re
 
 
 # FIXME handle error on gp --list
@@ -83,6 +84,71 @@ class AnalysisManager(CommandLineApp):
 
     def run(self):
         self.gp = GlobalPlatformProWrapper(log_verbosity=self.verbosity)
+
+
+
+
+class CardState(object):
+    # basically a parsed output from `gp --list`
+    def __init__(self, raw):
+        self.raw = raw
+        self.pkgs = None
+        self.applets = None
+        self._items = None
+
+    def _parse_raw(self):
+        if self._items is not None:
+            # cannot parse again!
+            return
+        self._items = []
+
+        # fmt: off
+        tag = re.compile(
+            r"(?P<type>ISD|APP|PKG):\s*"
+            r"(?P<aid>[A-Z0-9]+)\s*"
+            r"\((?P<state>\w+)\)"
+        )
+        prop = re.compile(
+            r"(?P<name>Privs|Version|Applet):\s*"
+            r"(?P<value>.*)"
+        )
+        # fmt: on
+
+        item = {}
+        flag = False
+        for line in self.raw.strip().split("\n"):
+            line = line.strip()
+
+            if not line:
+                continue
+
+            tag_match = tag.match(line)
+            if tag_match is not None:
+                _type = tag_match.group("type")
+                aid = tag_match.group("aid")
+                state = tag_match.group("state")
+
+                if flag:
+                    item = {}
+                item[_type] = {
+                    "AID": aid,
+                    "STATE": state,
+                    "ITEMS": {"Privs": [], "Version": [], "Applet": [],},
+                }
+                self._items.append(item)
+                flag = not flag
+                continue
+
+            prop_match = prop.match(line)
+            if prop_match is not None:
+                name = prop_match.group("name")
+                value = prop_match.group("value")
+                if name == "Privs":
+                    # if prop_match.group("value"):
+                    values = [x.strip() for x in value.split(",")]
+                    item[_type]["ITEMS"][name].extend(values)
+                else:
+                    item[_type]["ITEMS"][name].append(value)
 
 
 if __name__ == "__main__":
