a4067600b2e21c7da6e9abb792a4b2be35356f26
quapka <quapka@gmail.com>
2020-04-26 23:25:02 +0200

    Improve getting the root of the project

--- jsec/settings.py
+++ jsec/settings.py
@@ -1,6 +1,15 @@
 from pathlib import Path
+import os
 
-# kudos to https://stackoverflow.com/a/53465812/2377489
+
 def get_project_root():
-    """Returns project root folder."""
-    return Path(__file__).parent.parent
+    # kudos to https://stackoverflow.com/a/53465812/2377489
+    # first try to read the environemnt variable
+    env_root = Path(os.getenv("JCVM_ANALYSIS_HOME"))
+    relative_root = Path(__file__).parent.parent
+
+    if env_root:
+        # prefer user specified value in case of conflict
+        if env_root != relative_root:
+            return env_root
+    return relative_root
