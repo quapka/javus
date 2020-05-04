efee38ed2233c01178866e2bbec74d59c94ea84b
quapka <quapka@gmail.com>
2020-05-04 10:45:00 +0200

    Fix ant installation check

--- checks/check_ant.py
+++ checks/check_ant.py
@@ -2,6 +2,6 @@ import subprocess
 
 
 def check_ant_is_installed():
-    output = subprocess.check_output(["ant", "-version"], stdout=subprocess.PIPE)
+    output = subprocess.check_output(["ant", "-version"])
 
     assert output.decode("utf8").startswith("Apache Ant(TM)")
