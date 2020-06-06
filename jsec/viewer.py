1481c6ab5031dfbb2dcfc5698ffd5a1172e6ab55
quapka <quapka@gmail.com>
2020-06-06 17:52:38 +0200

    Simplify viewer development

--- jsec/viewer.py
+++ jsec/viewer.py
@@ -1,9 +1,33 @@
-from flask import Flask
-from flask import render_template
+import configparser
+import threading
+import webbrowser
+
+from flask import Flask, render_template
+
+from jsec.utils import MongoConnection
 
 app = Flask(__name__)
 
 
+def load_config():
+    config = configparser.ConfigParser()
+    config.read()
+
+
 @app.route("/")
+@app.route("/analysis/<id>")
 def index():
+    with MongoConnection():
+        pass
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    # the following lines are meant for testing and development
+    # the real server should be launched differently
+    app_thread = threading.Thread(target=app.run)
+    # TODO create URL dynamically
+    url = "localhost:5000"
+    browser_thread = threading.Thread(target=webbrowser.open, args=(url,))
+    app_thread.start()
+    browser_thread.start()
