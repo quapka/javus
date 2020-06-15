f3c510448b3731b93e051e708321ea5377ac302c
quapka <quapka@gmail.com>
2020-06-15 11:49:11 +0200

    Add another attack

---
+++ jsec/data/attacks/baload_bastore/bin/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+poc=baload_bastore
+genidx=1
+genarg=0
+
+../build.sh "$poc" "$genidx" "$genarg"
---
+++ jsec/data/attacks/baload_bastore/build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set poc=baload_bastore
+set genidx=1
+set genarg=0
+ 
+call ..\build.bat %poc% %genidx% %genarg%
