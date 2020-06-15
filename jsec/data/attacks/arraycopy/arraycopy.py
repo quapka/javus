850baf91d1b0e347ffbf0cef04b6b03dc997b2f3
quapka <quapka@gmail.com>
2020-06-15 19:30:34 +0200

    Fix order of applet installation

--- jsec/data/attacks/arraycopy/arraycopy.py
+++ jsec/data/attacks/arraycopy/arraycopy.py
@@ -3,19 +3,19 @@ class Stages:
     # FIXME rename comment to description?
     STAGES = [
         {
+            "name": "install",
+            "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
+            "comment": "The altered vulnerable applet",
+            # install should be required by default
+            "optional": False,
+        },
+        {
             # the name directly translates to the methods, that will be called
             "name": "install",
             # path relative to the directory of the attack
             "path": "build/{version}/com/se/applets/javacard/applets.cap",
             # comment/description of the stage
             "comment": "The base applet",
-            # install should be required by default
-            "optional": False,
-        },
-        {
-            "name": "install",
-            "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
-            "comment": "The altered vulnerable applet",
             # install should be required by default
             "optional": False,
         },
@@ -45,13 +45,13 @@ class Stages:
         },
         {
             "name": "uninstall",
-            "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
-            # TODO should be optional by default
+            "path": "build/{version}/com/se/applets/javacard/applets.cap",
             "optional": True,
         },
         {
             "name": "uninstall",
-            "path": "build/{version}/com/se/applets/javacard/applets.cap",
+            "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
+            # TODO should be optional by default
             "optional": True,
         },
     ]
