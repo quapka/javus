7faac1062229ea755ee75bf70c2b79e6b8f15a77
quapka <quapka@gmail.com>
2020-06-20 19:59:17 +0200

    Add entrypoint script for Docker

---
+++ bin/entrypoint-docker-jsec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if grep docker /proc/1/cgroup --quiet; then
+    echo "Running inside docker"
+    # TODO move to ./boostratp-live-container ?
+    echo "Starting pcscd deamon"
+    service pcscd start
+    echo "Checking pcscd daemon status:"
+    service pcscd status
+else
+    echo "Running outside of docker"
+fi
+
+if [ "$1" == "--DEBUG-DOCKER" ];
+then
+    echo "In a poor man's debug mode"
+    python3
+else
+    pipenv run python scripts/jsec $@
+fi
