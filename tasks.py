a1bc8be12191858cc2e62b60e01c697ecc6813e4
quapka <quapka@gmail.com>
2020-06-22 18:46:41 +0200

    Build always the lates Docker image

--- tasks.py
+++ tasks.py
@@ -140,7 +140,7 @@ def dock(c, local=True):
     # TODO rename task to something like build-docker-image
     # but only after tab completion for invoke commands is figure out
     r"""Build the newest docker image"""
-    cmd = ["docker", "build", "-t", "jsec", "."]
+    cmd = ["docker", "build", "--tag", "jsec:latest", "."]
     with cd(PROJECT_ROOT):
         c.run(" ".join(cmd))
 
