2bf8599325695dae3b82af1997bd2f2e1f5bee79
quapka <quapka@gmail.com>
2020-06-24 09:47:40 +0200

    Mark currently viewed stage

--- jsec/static/js/main.js
+++ jsec/static/js/main.js
@@ -1,4 +1,4 @@
-function getStageData(analysis_id, attack_name, stage_index, stage_name) {
+function getStageData(event, analysis_id, attack_name, stage_index, stage_name) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -10,4 +10,27 @@ function getStageData(analysis_id, attack_name, stage_index, stage_name) {
     // console.log("Using url: " + url);
     xhttp.open("GET", url, true);
     xhttp.send();
+
+    clearStagesBackground();
+    setBackground(event);
+
+}
+
+function setBackground(event) {
+    var element;
+    if ( event.target.tagName == 'SPAN' ) {
+        element = event.target.parentElement;
+    } else {
+        element = event.target;
+    }
+    console.log("Element", element);
+    element.style.background = '#ddd';
+}
+
+function clearStagesBackground() {
+  var stages = document.querySelectorAll('td.stage');
+  for (i = 0; i < stages.length; i++ ) {
+    stages[i].style.background = '';
+  }
+}
 }
