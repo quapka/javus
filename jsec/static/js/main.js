67106e53054000ccaf0c0b4834cb27571f0468d0
quapka <quapka@gmail.com>
2020-06-24 10:15:05 +0200

    Start working on copyAnalysisID function

--- jsec/static/js/main.js
+++ jsec/static/js/main.js
@@ -33,4 +33,19 @@ function clearStagesBackground() {
     stages[i].style.background = '';
   }
 }
+
+// TODO hmmmmmmmmmmmmm, this might be harder than expected
+function copyAnalysisID(textToCopy) {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
 }
