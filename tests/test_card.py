5dcddd3d0b6ae052b3761242dbd67cb633a9122c
quapka <quapka@gmail.com>
2020-06-24 16:51:52 +0200

    Test diffing two states

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,4 +1,5 @@
 import pytest
+from deepdiff import DeepDiff
 
 from jsec.card import CardState
 
@@ -150,3 +151,49 @@ PKG: 0011223344 (LOADED)
     state = CardState(raw)
     state.process()
     assert "0011223344AABB" in state.get_all_aids()
+
+
+def test__card_state_diff():
+    first_raw = """
+# Warning: no keys given, using default test key 404142434445464748494A4B4C4D4E4F
+ISD: A000000003000000 (OP_READY)
+     Privs:   SecurityDomain, CardLock, CardTerminate, CardReset, CVMManagement
+
+APP: A00000006303010C0101 (SELECTABLE)
+     Privs:
+"""
+
+    second_raw = """
+# Warning: no keys given, using default test key 404142434445464748494A4B4C4D4E4F
+ISD: A000000003000000 (OP_READY)
+     Privs:   SecurityDomain, CardLock, CardTerminate, CardReset, CVMManagement
+
+APP: A00000006303010C0101 (SELECTABLE)
+     Privs:
+
+PKG: 0011223344 (LOADED)
+     Version: 0.0
+     Applet:  0011223344AABB
+"""
+
+    first = CardState(first_raw)
+    first.process()
+    second = CardState(second_raw)
+    second.process()
+
+    diff = DeepDiff(first._items, second._items, ignore_order=True)
+    assert diff == {
+        "iterable_item_added": {
+            "root[2]": {
+                "PKG": {
+                    "AID": "0011223344",
+                    "ITEMS": {
+                        "Applet": ["0011223344AABB"],
+                        "Privs": [],
+                        "Version": ["0.0"],
+                    },
+                    "STATE": "LOADED",
+                }
+            }
+        }
+    }
