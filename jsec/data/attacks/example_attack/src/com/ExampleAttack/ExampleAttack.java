0f0c005e139cec3556d8a50b1f539248d3e17e25
quapka <quapka@gmail.com>
2020-07-04 09:57:59 +0200

    Update ExapmleAttack

--- jsec/data/attacks/example_attack/src/com/ExampleAttack/ExampleAttack.java
+++ jsec/data/attacks/example_attack/src/com/ExampleAttack/ExampleAttack.java
@@ -3,12 +3,12 @@ package com.examplepackage;
 import javacard.framework.*;
 
 public class ExampleAttack extends Applet {
-
-    private static final byte INS_SUCCESS = 0x01;
-    private static final byte INS_FAILURE = 0x02;
-
-    byte[] success = {(byte) 0x01, (byte) 0x02, (byte) 0x01, (byte) 0x02};
-    byte[] failure = {(byte) 0x80, (byte) 0x40, (byte) 0x80, (byte) 0x40};
+    // define the instructions
+    private static final byte INS_PREPARE = 0x01;
+    private static final byte INS_ATTACK = 0x02;
+    // define values for simulating an attack
+    byte[] prepare = {(byte) 0x01, (byte) 0x02, (byte) 0x01, (byte) 0x02};
+    byte[] secret = {(byte) 0x80, (byte) 0x40, (byte) 0x80, (byte) 0x40};
     short msgLen = 4;
 
     public static void install(byte[] array, short off, byte len) {
@@ -19,12 +19,18 @@ public class ExampleAttack extends Applet {
 
         byte[] buf = apdu.getBuffer();
         switch (buf[ISO7816.OFFSET_INS]) {
-            case INS_SUCCESS:
-                Util.arrayCopyNonAtomic(success, (short) 0, buf, (short) 0, msgLen);
+            case INS_PREPARE:
+                // Here we could do some preparation, maybe mangle some addresses etc.
+                // In this example we simply return an arbitrary array and won't do
+                // any setup.
+                Util.arrayCopyNonAtomic(prepare, (short) 0, buf, (short) 0, msgLen);
                 apdu.setOutgoingAndSend((short) 0, (short) 4);
                 break;
-            case INS_FAILURE:
-                Util.arrayCopyNonAtomic(failure, (short) 0, buf, (short) 0, msgLen);
+            case INS_ATTACK:
+                // Thanks to the previous instruction, that did some setup we can
+                // now call this instruction and e.g. read out some secret memory.
+                // Again, in this example we simply return a class array.
+                Util.arrayCopyNonAtomic(secret, (short) 0, buf, (short) 0, msgLen);
                 apdu.setOutgoingAndSend((short) 0, (short) 4);
                 break;
             default:
